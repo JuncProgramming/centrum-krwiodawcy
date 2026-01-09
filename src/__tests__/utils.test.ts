759af70db31aba3b6ee4dd55b83a180c2d1eb40a
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-01-09 20:54:48 +0100

    Reduce the test chaos by using it.each in the utils test suite for some of the functions

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -9,30 +9,21 @@ import type { Donation } from '@/types';
 
 describe('utils', () => {
   describe('normalizeType', () => {
-    it('should return "krew_pelna" when "Krew pełna" or "krew_pelna" is passed in', () => {
-      expect(normalizeType('Krew pełna')).toBe('krew_pelna');
-      expect(normalizeType('krew_pelna')).toBe('krew_pelna');
-    });
-
-    it('should return "osocze" when "Osocze" or "osocze" is passed in', () => {
-      expect(normalizeType('Osocze')).toBe('osocze');
-      expect(normalizeType('osocze')).toBe('osocze');
-    });
-
-    it('should return "plytki_krwi" when "Płytki krwi" or "plytki_krwi" is passed in', () => {
-      expect(normalizeType('Płytki krwi')).toBe('plytki_krwi');
-      expect(normalizeType('plytki_krwi')).toBe('plytki_krwi');
-    });
-
-    it('should return "krew_pelna" when an empty string is passed in', () => {
-      expect(normalizeType('')).toBe('krew_pelna');
-    });
-
-    it('should return "krew_pelna" when string other than the set strings is passed in', () => {
-      expect(normalizeType('random')).toBe('krew_pelna');
-      expect(normalizeType('abc')).toBe('krew_pelna');
-      expect(normalizeType('123')).toBe('krew_pelna');
-    });
+    it.each([
+      { input: 'Krew pełna', expected: 'krew_pelna' },
+      { input: 'krew_pelna', expected: 'krew_pelna' },
+      { input: 'Osocze', expected: 'osocze' },
+      { input: 'osocze', expected: 'osocze' },
+      { input: 'Płytki krwi', expected: 'plytki_krwi' },
+      { input: 'plytki_krwi', expected: 'plytki_krwi' },
+      { input: '', expected: 'krew_pelna' },
+      { input: 'random', expected: 'krew_pelna' }
+    ])(
+      'should return $expected when $input is passed in',
+      ({ input, expected }) => {
+        expect(normalizeType(input)).toBe(expected);
+      }
+    );
   });
 
   describe('calculateNextDonationDate', () => {
@@ -167,139 +158,108 @@ describe('utils', () => {
       expect(hundredProgressResult.progress).toBe(100);
     });
 
-    it('should display gender note only for "Krew pełna" to "Krew pełna" scenario', () => {
-      const krewToKrewResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Krew pełna',
-        'Krew pełna'
-      );
-      const krewToOsoczeResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Krew pełna',
-        'Osocze'
-      );
-      const krewToPlytkiResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Krew pełna',
-        'Płytki krwi'
-      );
-      const osoczeToKrewResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Osocze',
-        'Krew pełna'
-      );
-      const osoczeToOsoczeResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Osocze',
-        'Osocze'
-      );
-      const osoczeToPlytkiResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Osocze',
-        'Płytki krwi'
-      );
-      const plytkiToKrewResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Płytki krwi',
-        'Krew pełna'
-      );
-      const plytkiToOsoczeResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Płytki krwi',
-        'Osocze'
-      );
-      const plytkiToPlytkiResult = calculateNextDonationDate(
-        '2025-01-01',
-        'Płytki krwi',
-        'Płytki krwi'
-      );
+    it.each([
+      {
+        params: { date: '2025-01-01', last: 'Krew pełna', next: 'Krew pełna' },
+        expected: true
+      },
+      {
+        params: { date: '2025-01-01', last: 'Krew pełna', next: 'Osocze' },
+        expected: false
+      },
+      {
+        params: { date: '2025-01-01', last: 'Krew pełna', next: 'Płytki krwi' },
+        expected: false
+      },
+      {
+        params: { date: '2025-01-01', last: 'Osocze', next: 'Krew pełna' },
+        expected: false
+      },
+      {
+        params: { date: '2025-01-01', last: 'Osocze', next: 'Osocze' },
+        expected: false
+      },
+      {
+        params: { date: '2025-01-01', last: 'Osocze', next: 'Płytki krwi' },
+        expected: false
+      },
+      {
+        params: { date: '2025-01-01', last: 'Płytki krwi', next: 'Krew pełna' },
+        expected: false
+      },
+      {
+        params: { date: '2025-01-01', last: 'Płytki krwi', next: 'Osocze' },
+        expected: false
+      },
+      {
+        params: {
+          date: '2025-01-01',
+          last: 'Płytki krwi',
+          next: 'Płytki krwi'
+        },
+        expected: false
+      }
+    ])(
+      'should display gender note only for $last to $next scenario',
+      ({ params: { date, last, next }, expected }) => {
+        const result = calculateNextDonationDate(date, last, next);
+        expect(result.showGenderNote).toBe(expected);
+      }
+    );
+  });
+});
 
-      expect(krewToKrewResult.showGenderNote).toBe(true);
-      expect(krewToOsoczeResult.showGenderNote).toBe(false);
-      expect(krewToPlytkiResult.showGenderNote).toBe(false);
-      expect(osoczeToKrewResult.showGenderNote).toBe(false);
-      expect(osoczeToOsoczeResult.showGenderNote).toBe(false);
-      expect(osoczeToPlytkiResult.showGenderNote).toBe(false);
-      expect(plytkiToKrewResult.showGenderNote).toBe(false);
-      expect(plytkiToOsoczeResult.showGenderNote).toBe(false);
-      expect(plytkiToPlytkiResult.showGenderNote).toBe(false);
+describe('calculateTaxRelief', () => {
+  const fakeDonations = [
+    { date: '2025-01-10', type: 'Krew pełna', amount: 450 },
+    { date: '2025-05-12', type: 'Osocze', amount: 650 },
+    { date: '2025-05-12', type: 'Płytki krwi', amount: 500 },
+    { date: '2024-12-20', type: 'Krew pełna', amount: 450 },
+    { date: '2023-07-02', type: 'Krew pełna', amount: 225 },
+    { date: '2022-01-01', type: 'Krew pełna' } as unknown as Donation,
+    { date: '2022-01-01', type: 'Osocze' } as unknown as Donation,
+    { date: '2022-01-01', type: 'Płytki krwi' } as unknown as Donation
+  ] as unknown as Donation[];
+
+  it('should return 0 for both the count and taxRelief when there are no donations in a certain year', () => {
+    expect(calculateTaxRelief(fakeDonations, 2020)).toEqual({
+      amount: 0,
+      donationCount: 0
     });
   });
 
-  describe('calculateTaxRelief', () => {
-    const fakeDonations = [
-      { date: '2025-01-10', type: 'Krew pełna', amount: 450 },
-      { date: '2025-05-12', type: 'Osocze', amount: 650 },
-      { date: '2025-05-12', type: 'Płytki krwi', amount: 500 },
-      { date: '2024-12-20', type: 'Krew pełna', amount: 450 },
-      { date: '2023-07-02', type: 'Krew pełna', amount: 225 },
-      { date: '2022-01-01', type: 'Krew pełna' } as unknown as Donation,
-      { date: '2022-01-01', type: 'Osocze' } as unknown as Donation,
-      { date: '2022-01-01', type: 'Płytki krwi' } as unknown as Donation
-    ] as unknown as Donation[];
-
-    it('should return 0 for both the count and taxRelief when there are no donations in a certain year', () => {
-      expect(calculateTaxRelief(fakeDonations, 2020)).toEqual({
-        amount: 0,
-        donationCount: 0
-      });
-    });
-
-    it('should return correct count and taxRelief only for the selected year', () => {
-      expect(calculateTaxRelief(fakeDonations, 2025)).toEqual({
-        amount: 208,
-        donationCount: 3
-      });
-    });
-
-    it('should return correct count and taxRelief when the amount is not the default amount', () => {
-      expect(calculateTaxRelief(fakeDonations, 2023)).toEqual({
-        amount: 29.25,
-        donationCount: 1
-      });
-    });
-
-    it('should return correct count and taxRelief when there is no amount provided (fallback to default values)', () => {
-      expect(calculateTaxRelief(fakeDonations, 2022)).toEqual({
-        amount: 175.5,
-        donationCount: 3
-      });
+  it('should return correct count and taxRelief only for the selected year', () => {
+    expect(calculateTaxRelief(fakeDonations, 2025)).toEqual({
+      amount: 208,
+      donationCount: 3
     });
   });
 
-  describe('getDonationsWordForm', () => {
-    it('should return "donacji" when the number passed in is 0', () => {
-      expect(getDonationsWordForm(0)).toBe('donacji');
+  it('should return correct count and taxRelief when the amount is not the default amount', () => {
+    expect(calculateTaxRelief(fakeDonations, 2023)).toEqual({
+      amount: 29.25,
+      donationCount: 1
     });
+  });
 
-    it('should return "donacja" when the number passed in is 1', () => {
-      expect(getDonationsWordForm(1)).toBe('donacja');
+  it('should return correct count and taxRelief when there is no amount provided (fallback to default values)', () => {
+    expect(calculateTaxRelief(fakeDonations, 2022)).toEqual({
+      amount: 175.5,
+      donationCount: 3
     });
+  });
+});
 
-    it('should return "donacje" when the number passed in is 2, 3 or 4', () => {
-      expect(getDonationsWordForm(2)).toBe('donacje');
-      expect(getDonationsWordForm(3)).toBe('donacje');
-      expect(getDonationsWordForm(4)).toBe('donacje');
-    });
-
-    it('should return "donacji" when the number passed in is between 5 and 21', () => {
-      expect(getDonationsWordForm(5)).toBe('donacji');
-      expect(getDonationsWordForm(12)).toBe('donacji');
-      expect(getDonationsWordForm(21)).toBe('donacji');
-    });
-
-    it('should return "donacje" when the number passed in has the second digit as a 2, 3 or 4', () => {
-      expect(getDonationsWordForm(22)).toBe('donacje');
-      expect(getDonationsWordForm(23)).toBe('donacje');
-      expect(getDonationsWordForm(24)).toBe('donacje');
-
-      expect(getDonationsWordForm(32)).toBe('donacje');
-      expect(getDonationsWordForm(43)).toBe('donacje');
-      expect(getDonationsWordForm(54)).toBe('donacje');
-
-      expect(getDonationsWordForm(62)).toBe('donacje');
-      expect(getDonationsWordForm(73)).toBe('donacje');
-      expect(getDonationsWordForm(84)).toBe('donacje');
-    });
+describe('getDonationsWordForm', () => {
+  it.each([
+    { count: 0, expected: 'donacji' },
+    { count: 1, expected: 'donacja' },
+    { count: 2, expected: 'donacje' },
+    { count: 5, expected: 'donacji' },
+    { count: 12, expected: 'donacji' },
+    { count: 22, expected: 'donacje' },
+    { count: 25, expected: 'donacji' }
+  ])('should return $expected for number $count', ({ count, expected }) => {
+    expect(getDonationsWordForm(count)).toBe(expected);
   });
 });
