64ec0422d067d3cad1f4efdcb8e5f80a81845d88
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-01-31 18:38:07 +0100

    Updates

--- assets/chevron-down-Da9YKxNp.js
+++ assets/chevron-down-Da9YKxNp.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-K7p4WVK1.js";/**
+import{c as o}from"./index-DfqlAED3.js";/**
  * @license lucide-react v0.545.0 - ISC
  *
  * This source code is licensed under the ISC license.
