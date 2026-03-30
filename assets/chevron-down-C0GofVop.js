bb23c3ec3afcdc8aae35064d78c4856c0c50404f
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-03-30 17:18:02 +0200

    Updates

--- assets/chevron-down-C0GofVop.js
+++ assets/chevron-down-C0GofVop.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-DfqlAED3.js";/**
+import{c as o}from"./index-CSPUMC_S.js";/**
  * @license lucide-react v0.545.0 - ISC
  *
  * This source code is licensed under the ISC license.
