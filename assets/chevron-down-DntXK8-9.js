c6b8a9cf147719f0998ee78a36981b95cddd8015
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-04-06 10:19:09 +0200

    Updates

--- assets/chevron-down-DntXK8-9.js
+++ assets/chevron-down-DntXK8-9.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-CSPUMC_S.js";/**
+import{c as o}from"./index-BLaGAX18.js";/**
  * @license lucide-react v0.545.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/circle-check-Bk5E5E4Y.js
+++ assets/circle-check-Bk5E5E4Y.js
@@ -1,4 +1,4 @@
-import{c}from"./index-CSPUMC_S.js";/**
+import{c}from"./index-BLaGAX18.js";/**
  * @license lucide-react v0.545.0 - ISC
  *
  * This source code is licensed under the ISC license.
